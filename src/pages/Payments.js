7ac8bc8f1b7e21295f378bbaa9d70c851609278d
fronix <oscar.martin@tieto.com>
2021-01-10 00:49:43 +0100

    fix api url

--- src/pages/Payments.js
+++ src/pages/Payments.js
@@ -8,7 +8,7 @@ import getMonth from '../utils/date';
 const fetcher = (...args) => fetch(...args).then(res => res.json())
 
 const Payments = () => {
-  const { data: payments, error, mutate } = useSWR('http://localhost:3001/users', fetcher)
+  const { data: payments, error, mutate } = useSWR('https://darthvader.fronix.se/payment-api/users', fetcher)
   
   const TABLE_HEADER = [
     "Jan", "Feb", "Mar",
